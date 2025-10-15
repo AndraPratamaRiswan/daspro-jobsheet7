a9508d2593988091bcff8c71199b04df5318ad29
AndraPratamaRiswan <andra.pratama@riswan.com>
2025-10-15 11:45:47 +0700

    perubahan lulus & tdk lulus while

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -4,9 +4,22 @@ public class SiakadFor05 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        int mhs = 10, lulus = 0, tdkLulus = 0, i;
+        i=1;
         double nilai, tertinggi = 0, terendah = 100;
-        for (int i = 1; i < 10; i++){
-            System.out.print("Masukan nilai mahasiswa ke-" + i + ": ");
+        //for (int i = 1; i < 10; i++){
+            //System.out.print("Masukan nilai mahasiswa ke-" + i + ": ");
+            //nilai = sc.nextDouble();
+           // if (nilai > tertinggi){
+                //tertinggi = nilai;
+            //}
+            //if (nilai < terendah){
+               // terendah = nilai;
+            //}
+        //}
+
+        while (i <= mhs){
+            System.out.print("Masukan Nilai Mahasiswa ke-"+ i +": ");
             nilai = sc.nextDouble();
             if (nilai > tertinggi){
                 tertinggi = nilai;
@@ -14,8 +27,16 @@ public class SiakadFor05 {
             if (nilai < terendah){
                 terendah = nilai;
             }
+            if (nilai >= 60){
+                lulus++;
+            }else {
+                tdkLulus++;
+            }
+            i++;
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Banyak mahasiswa yang lulus: "+ lulus);
+        System.out.println("Banyak mahasiswa yang tidak lulus: "+ tdkLulus);
     }
 }

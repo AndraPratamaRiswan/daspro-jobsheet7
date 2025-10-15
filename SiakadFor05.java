feb9d005e92b67583170cfa8ba7fde9e5a464159
AndraPratamaRiswan <andra.pratama@riswan.com>
2025-10-15 11:53:47 +0700

    percobaan lulus&tdk lulus for

--- SiakadFor05.java
+++ SiakadFor05.java
@@ -4,21 +4,10 @@ public class SiakadFor05 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int mhs = 10, lulus = 0, tdkLulus = 0, i;
-        i=1;
+        int mhs = 10, lulus = 0, tdkLulus = 0;
         double nilai, tertinggi = 0, terendah = 100;
-        //for (int i = 1; i < 10; i++){
-            //System.out.print("Masukan nilai mahasiswa ke-" + i + ": ");
-            //nilai = sc.nextDouble();
-           // if (nilai > tertinggi){
-                //tertinggi = nilai;
-            //}
-            //if (nilai < terendah){
-               // terendah = nilai;
-            //}
-        //}
-
-        while (i <= mhs){
+        
+        for (int i = 1; i <= mhs; i++){
             System.out.print("Masukan Nilai Mahasiswa ke-"+ i +": ");
             nilai = sc.nextDouble();
             if (nilai > tertinggi){
@@ -32,7 +21,6 @@ public class SiakadFor05 {
             }else {
                 tdkLulus++;
             }
-            i++;
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);

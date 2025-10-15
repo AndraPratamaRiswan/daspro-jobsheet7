e3d748375b152fa2f160c91f93d3f7c204247a26
AndraPratamaRiswan <andra.pratama@riswan.com>
2025-10-15 14:31:58 +0700

    tugas2 jobsheet7

---
+++ tugas2jb7_05.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class tugas2jb7_05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jenis, durasi, total = 0;
+        
+        do{
+            System.out.print("Masukan jenis kendaraan (1. Mobil, 2. Motor, 0. Keluar): ");
+            jenis = sc.nextInt();
+
+            if (jenis == 1 || jenis == 2){
+                System.out.print("Masukan durasi parkir (jam): ");
+                durasi = sc.nextInt();
+
+                if (durasi > 5){
+                    total += 12500;
+                }else{
+                    if (jenis == 1) {
+                        total += durasi * 3000;
+                    }else if (jenis == 2) {
+                        total += durasi * 2000;
+                    }
+                }
+            }else if (jenis != 0) {
+                System.out.println("Jenis kendaraan tidak valid!");
+            }
+            System.out.println();
+        } while (jenis != 0);
+
+        System.out.println(("Total pembayaran parkir hari ini: Rp. "+ total));
+    }
+}
